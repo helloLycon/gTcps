b026021af46d71c3888f16344a057b0ab304b57a
lycon <lycon123@foxmail.com>
2021-02-04 09:37:09 +0800

    单纯接收，发包测试用的

--- cliRoutine.cpp
+++ cliRoutine.cpp
@@ -18,27 +18,28 @@ set<int> fdSet;
 pthread_mutex_t fdMutex = PTHREAD_MUTEX_INITIALIZER;
 
 void * clientRoutine(void *arg) {
+    static long long packetCnter = 0;
     extern int quiet;
     const CliMsg *pcli = (const CliMsg *)arg;
     CliMsg cli = *pcli;
     FILE *fp = fdopen(cli.fd, "r+");
-    char line[1024];
+    char line[10240];
 
     pthread_mutex_lock(&fdMutex);
     fdSet.insert(cli.fd);
     printf("%s:%d connected! (%d)\n", inet_ntoa(cli.addr.sin_addr), cli.addr.sin_port, fdSet.size());
     pthread_mutex_unlock(&fdMutex);
     fprintf(fp, "hello~\r\n");
-    for(cli.msgCount=0; fgets(line, sizeof line, fp) ;) {
-        if( !strncmp("quit", line, 4) ) {
-            fprintf(fp, "\r\nSee you~\r\n");
-            goto quit;
-        }
+    for(cli.msgCount=0; fread(line, sizeof line,1, fp) ;) {
         cli.msgCount++;
-        printf("%s:%d - [%d]%s", inet_ntoa(cli.addr.sin_addr), cli.addr.sin_port, cli.msgCount, line);
+        packetCnter++;
+        if(0 == (packetCnter % 10000)) {
+            printf("%ld received\n", packetCnter);
+        }
+        /*printf("%s:%d - [%d]%s", inet_ntoa(cli.addr.sin_addr), cli.addr.sin_port, cli.msgCount, line);
         if(!quiet) {
             fprintf(fp, "You Said: %s", line);
-        }
+        }*/
     }
 
 quit:
